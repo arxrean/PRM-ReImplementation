3db0e06ab6aa8360c1c8c724f2a01ff6135ab318
arxrean <602052254@qq.com>
2019-06-13 22:18:09 +0800

    abc

---
+++ PRM/loader/dataset.py
@@ -0,0 +1,99 @@
+import os
+from collections import OrderedDict
+from typing import Tuple, List, Dict, Union, Callable, Optional
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets, transforms
+from PIL import Image
+import pdb
+
+
+def pascal_voc_object_categories(query: Optional[Union[int, str]] = None) -> Union[int, str, List[str]]:
+    """PASCAL VOC dataset class names.
+    """
+
+    categories = [
+        'aeroplane', 'bicycle', 'bird', 'boat',
+        'bottle', 'bus', 'car', 'cat', 'chair',
+        'cow', 'diningtable', 'dog', 'horse',
+        'motorbike', 'person', 'pottedplant',
+        'sheep', 'sofa', 'train', 'tvmonitor']
+
+    if query is None:
+        return categories
+    else:
+        for idx, val in enumerate(categories):
+            if isinstance(query, int) and idx == query:
+                return val
+            elif val == query:
+                return idx
+
+
+class VOC_Classification(Dataset):
+    """Dataset for PASCAL VOC classification.
+    """
+
+    def __init__(self, data_dir, dataset, split, classes, transform=None, target_transform=None):
+        self.data_dir = data_dir
+        self.dataset = dataset
+        self.split = split
+        self.image_dir = os.path.join(data_dir, dataset, 'JPEGImages')
+        assert os.path.isdir(
+            self.image_dir), 'Could not find image folder "%s".' % self.image_dir
+        self.gt_path = os.path.join(
+            self.data_dir, self.dataset, 'ImageSets', 'Main')
+        assert os.path.isdir(
+            self.gt_path), 'Could not find ground truth folder "%s".' % self.gt_path
+        self.transform = transform
+        self.target_transform = target_transform
+        self.classes = classes
+        self.image_labels = self._read_annotations(self.split)
+
+    def _read_annotations(self, split):
+        class_labels = OrderedDict()
+        num_classes = len(self.classes)
+        if os.path.exists(os.path.join(self.gt_path, split + '.txt')):
+            for class_idx in range(num_classes):
+                filename = os.path.join(
+                    self.gt_path, self.classes[class_idx] + '_' + split + '.txt')
+                with open(filename, 'r') as f:
+                    for line in f:
+                        name, label = line.split()
+                        if name not in class_labels:
+                            class_labels[name] = np.zeros(num_classes)
+                        class_labels[name][class_idx] = int(label)
+        else:
+            raise NotImplementedError(
+                'Invalid "%s" split for PASCAL %s classification task.' % (split, self.dataset))
+
+        return list(class_labels.items())
+
+    def __getitem__(self, index):
+        filename, target = self.image_labels[index]
+        target = torch.from_numpy(target).float()
+        img = Image.open(os.path.join(
+            self.image_dir, filename + '.jpg')).convert('RGB')
+        if self.transform:
+            img = self.transform(img)
+        if self.target_transform:
+            target = self.target_transform(target)
+        return filename, img, target
+
+    def __len__(self):
+        return len(self.image_labels)
+
+
+def pascal_voc_classification(
+        split: str,
+        data_dir: str,
+        year: int = 2007,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None) -> Dataset:
+    """PASCAL VOC dataset.
+    """
+
+    object_categories = pascal_voc_object_categories()
+    dataset = 'VOC' + str(year)
+    return VOC_Classification(data_dir, dataset, split, object_categories, transform, target_transform)
---
+++ PRM/train_cls.py
@@ -0,0 +1,87 @@
+import argparse
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets, transforms
+import torch.nn as nn
+
+from loader.dataset import pascal_voc_classification
+from model.peak_net import peak_response_mapping
+from model.backbone import fc_resnet50
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--session_name", default="peak_cls_train", type=str)
+    # data
+    parser.add_argument(
+        "--voc12_root", default='/u/zkou2/Data/VOCdevkit', type=str)
+    parser.add_argument("--crop_size", default=448, type=int)
+    # config
+    parser.add_argument("--num_workers", default=8, type=int)
+    parser.add_argument("--batch_size", default=16, type=int)
+    parser.add_argument("--max_epoches", default=20, type=int)
+    # train
+    parser.add_argument("--lr", default=0.1, type=float)
+    parser.add_argument("--wt_dec", default=5e-4, type=float)
+    # save
+    parser.add_argument("--save_weights", default='save/weights', type=str)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def get_finetune_optimizer(args, model, epoch):
+    lr = args.lr
+    weight_list = []
+    bias_list = []
+    last_weight_list = []
+    last_bias_list = []
+    for name, value in model.named_parameters():
+        if 'classifier' in name:
+            if 'weight' in name:
+                last_weight_list.append(value)
+            elif 'bias' in name:
+                last_bias_list.append(value)
+        else:
+            if 'weight' in name:
+                weight_list.append(value)
+            elif 'bias' in name:
+                bias_list.append(value)
+
+    opt = optim.SGD([{'params': weight_list, 'lr': lr},
+                     {'params': bias_list, 'lr': lr},
+                     {'params': last_weight_list, 'lr': lr, 'weight_decay': 1e-4},
+                     {'params': last_bias_list, 'lr': lr, 'weight_decay': 1e-4}], momentum=0.9)
+
+    return opt
+
+
+def train(args):
+    train_transform = transforms.Compose([
+        transforms.Resize((448, 448)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    train_dataset = pascal_voc_classification(
+        split='trainval', data_dir=args.voc12_root, year=2012, transform=train_transform)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, num_workers=args.num_workers,
+                              pin_memory=True, drop_last=False, shuffle=True)
+
+    model = peak_response_mapping(
+        backbone=fc_resnet50(), sub_pixel_locating_factor=8)
+    model = model.cuda()
+
+    for epoch in range(args.max_epoches):
+	    for iter, pack in enumerate(train_loader):
+	        imgs = pack[1].cuda()
+	        labels = pack[2].cuda()
+
+	        aggregation = model.forward(imgs)
+	        pass
+
+
+if __name__ == '__main__':
+    args = parse()
+    train(args)
