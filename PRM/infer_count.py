c1d6695746eec22e619cff199b2bdeeb24e3b970
arxrean <602052254@qq.com>
2019-06-24 19:07:15 +0800

    abc

--- PRM/infer_count.py
+++ PRM/infer_count.py
@@ -18,35 +18,35 @@ warnings.filterwarnings("ignore")
 
 
 def parse():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--session_name", default="peak_cls_train", type=str)
-    # data
-    parser.add_argument(
-        "--voc12_root", default='/u/zkou2/Data/VOCdevkit/VOC2012', type=str)
-    parser.add_argument("--crop_size", default=448, type=int)
-    # config
-    parser.add_argument("--batch_size", default=16, type=int)
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--session_name", default="peak_cls_train", type=str)
+	# data
+	parser.add_argument(
+		"--voc12_root", default='/u/zkou2/Data/VOCdevkit/VOC2012', type=str)
+	parser.add_argument("--crop_size", default=448, type=int)
+	# config
+	parser.add_argument("--batch_size", default=16, type=int)
 
-    # save
-    parser.add_argument("--save_weights", default='save/weights', type=str)
+	# save
+	parser.add_argument("--save_weights", default='save/weights', type=str)
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    return args
+	return args
 
 
 def voc12_train_count(args):
-    train_transform = transforms.Compose([
-        transforms.Resize((448, 448)),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-    dataset = PascalVOCDetection(images_folder_path='/u/zkou2/Data/VOCdevkit/VOC2012',
-                                 annotation_json='/u/zkou2/Data/VOCdevkit/PASCAL_VOC_JSON/pascal_train2012.json',
-                                 image_transform=train_transform)
-    train_loader = DataLoader(dataset, batch_size=16, num_workers=0,
+	train_transform = transforms.Compose([
+		transforms.Resize((448, 448)),
+		transforms.ToTensor(),
+		transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+	])
+	dataset = PascalVOCDetection(images_folder_path='/u/zkou2/Data/VOCdevkit/VOC2012',
+								 annotation_json='/u/zkou2/Data/VOCdevkit/PASCAL_VOC_JSON/pascal_train2012.json',
+								 image_transform=train_transform)
+	train_loader = DataLoader(dataset, batch_size=16, num_workers=0,
 							  pin_memory=True, drop_last=False, shuffle=True)
-    model = peak_response_mapping(
+	model = peak_response_mapping(
 		backbone=fc_resnet50(), sub_pixel_locating_factor=8)
 	model = model.cuda()
 	model.load_state_dict(torch.load('./save/weights/peak_cls_train.pt'))
@@ -59,5 +59,5 @@ def voc12_train_count(args):
 
 
 if __name__ == '__main__':
-    args = parse()
-    voc12_train_count(args)
+	args = parse()
+	voc12_train_count(args)
