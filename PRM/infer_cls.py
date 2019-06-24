e684d42207a1dce21b8bb11e02d16b6b21e02b81
arxrean <602052254@qq.com>
2019-06-24 15:36:52 +0800

    abc

--- PRM/infer_cls.py
+++ PRM/infer_cls.py
@@ -25,161 +25,161 @@ warnings.filterwarnings("ignore")
 
 
 def parse():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--session_name", default="peak_cls_train", type=str)
-    # data
-    parser.add_argument(
-        "--voc12_root", default='/u/zkou2/Data/VOCdevkit', type=str)
-    parser.add_argument("--crop_size", default=448, type=int)
-    # config
-    parser.add_argument("--batch_size", default=16, type=int)
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--session_name", default="peak_cls_train", type=str)
+	# data
+	parser.add_argument(
+		"--voc12_root", default='/u/zkou2/Data/VOCdevkit', type=str)
+	parser.add_argument("--crop_size", default=448, type=int)
+	# config
+	parser.add_argument("--batch_size", default=16, type=int)
 
-    # save
-    parser.add_argument("--save_weights", default='save/weights', type=str)
+	# save
+	parser.add_argument("--save_weights", default='save/weights', type=str)
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    return args
+	return args
 
 
 def random_color():
-    _HEX = '0123456789ABCDEF'
+	_HEX = '0123456789ABCDEF'
 
-    return '#' + ''.join(random.choice(_HEX) for _ in range(6))
+	return '#' + ''.join(random.choice(_HEX) for _ in range(6))
 
 
 def plot_points(raw_img, valid_peak_list, class_names):
-    plt.imshow(raw_img)
-    plt.savefig('raw_img.png')
-    plt.close()
+	plt.imshow(raw_img)
+	plt.savefig('raw_img.png')
+	plt.close()
 
-    plt.figure(figsize=(10, 10))
+	plt.figure(figsize=(10, 10))
 
-    peaks = valid_peak_list.cpu().numpy()
-    classes = list(set([x[1] for x in peaks]))
-    class2color = dict()
-    for c in classes:
-        class2color[c] = random_color()
+	peaks = valid_peak_list.cpu().numpy()
+	classes = list(set([x[1] for x in peaks]))
+	class2color = dict()
+	for c in classes:
+		class2color[c] = random_color()
 
-    for p in peaks:
-        plt.scatter([p[3]], [p[2]], s=75, c=class2color[p[1]],
-                    label='{}'.format(class_names[p[1]]))
+	for p in peaks:
+		plt.scatter([p[3]], [p[2]], s=75, c=class2color[p[1]],
+					label='{}'.format(class_names[p[1]]))
 
-    plt.xticks(np.arange(0, 14, 1))
-    plt.yticks(np.arange(0, 14, 1))
+	plt.xticks(np.arange(0, 14, 1))
+	plt.yticks(np.arange(0, 14, 1))
 
-    plt.savefig('tmp_img.png')
-    plt.close()
+	plt.savefig('tmp_img.png')
+	plt.close()
 
 
 def sample():
-    # run_tasks('./config.yml')
-    class_names = modules.pascal_voc_object_categories()
+	# run_tasks('./config.yml')
+	class_names = modules.pascal_voc_object_categories()
 
-    image_size = 448
-    # image pre-processor
-    transformer = modules.image_transform(
+	image_size = 448
+	# image pre-processor
+	transformer = modules.image_transform(
 
-        image_size=[image_size, image_size],
-        augmentation=dict(),
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225])
+		image_size=[image_size, image_size],
+		augmentation=dict(),
+		mean=[0.485, 0.456, 0.406],
+		std=[0.229, 0.224, 0.225])
 
-    backbone = modules.fc_resnet50(num_classes=20, pretrained=False)
-    model = modules.peak_response_mapping(backbone)
+	backbone = modules.fc_resnet50(num_classes=20, pretrained=False)
+	model = modules.peak_response_mapping(backbone)
 
-    state = torch.load('./save/weights/peak_cls_train.pt')
-    # new_dict=dict()
-    # for k,v in state['model'].items():
-    #   new_dict[k[7:]]=v
-    model.load_state_dict(state)
-    model = model.cuda()
+	state = torch.load('./save/weights/peak_cls_train.pt')
+	# new_dict=dict()
+	# for k,v in state['model'].items():
+	#   new_dict[k[7:]]=v
+	model.load_state_dict(state)
+	model = model.cuda()
 
-    idx = 0
-    raw_img = PIL.Image.open('./data/sample%d.jpg' % idx).convert('RGB')
-    input_var = transformer(raw_img).unsqueeze(0).cuda().requires_grad_()
-    with open('./data/sample%d.json' % idx, 'r') as f:
-        proposals = list(map(modules.rle_decode, json.load(f)))
+	idx = 0
+	raw_img = PIL.Image.open('./data/sample%d.jpg' % idx).convert('RGB')
+	input_var = transformer(raw_img).unsqueeze(0).cuda().requires_grad_()
+	with open('./data/sample%d.json' % idx, 'r') as f:
+		proposals = list(map(modules.rle_decode, json.load(f)))
 
-    model = model.eval()
-    confidence = model(input_var)
-    for idx in range(len(class_names)):
-        if confidence.data[0, idx] > 0:
-            print('    [class_idx: %d] %s (%.2f)' %
-                  (idx, class_names[idx], confidence[0, idx]))
+	model = model.eval()
+	confidence = model(input_var)
+	for idx in range(len(class_names)):
+		if confidence.data[0, idx] > 0:
+			print('    [class_idx: %d] %s (%.2f)' %
+				  (idx, class_names[idx], confidence[0, idx]))
 
-    model = model.inference()
-    aggregation, class_response_maps, valid_peak_list, peak_response_maps = model(
-        input_var)
-    plot_points(raw_img, valid_peak_list, class_names)
+	model = model.inference()
+	aggregation, class_response_maps, valid_peak_list, peak_response_maps = model(
+		input_var)
+	plot_points(raw_img, valid_peak_list, class_names)
 
 
 def train_infer(args):
-    class_names = modules.pascal_voc_object_categories()
+	class_names = modules.pascal_voc_object_categories()
 
-    train_transform = transforms.Compose([
-        transforms.Resize((448, 448)),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
+	train_transform = transforms.Compose([
+		transforms.Resize((448, 448)),
+		transforms.ToTensor(),
+		transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+	])
 
-    train_dataset = pascal_voc_classification(
-        split='trainval', data_dir=args.voc12_root, year=2012, transform=train_transform)
-    train_loader = DataLoader(train_dataset, batch_size=16, num_workers=0,
-                              pin_memory=True, drop_last=False, shuffle=True)
+	train_dataset = pascal_voc_classification(
+		split='trainval', data_dir=args.voc12_root, year=2012, transform=train_transform)
+	train_loader = DataLoader(train_dataset, batch_size=16, num_workers=0,
+							  pin_memory=True, drop_last=False, shuffle=True)
 
-    model = peak_response_mapping(
-        backbone=fc_resnet50(), sub_pixel_locating_factor=8)
-    model = model.cuda()
-    model.load_state_dict(torch.load('./save/weights/peak_cls_train.pt'))
+	model = peak_response_mapping(
+		backbone=fc_resnet50(), sub_pixel_locating_factor=8)
+	model = model.cuda()
+	model.load_state_dict(torch.load('./save/weights/peak_cls_train.pt'))
 
-    results = []
-    gt = []
-    with torch.no_grad():
-        for iter, pack in enumerate(tqdm(train_loader)):
-        	if iter==3:
-        		break
-            imgs = pack[1].cuda()
-            labels = pack[2].cuda()
+	results = []
+	gt = []
+	with torch.no_grad():
+		for iter, pack in enumerate(tqdm(train_loader)):
+			if iter==3:
+				break
+			imgs = pack[1].cuda()
+			labels = pack[2].cuda()
 
-            aggregation = model.forward(imgs)
-            results.append(aggregation.detach().cpu().numpy())
-            gt.append(labels.cpu().numpy())
+			aggregation = model.forward(imgs)
+			results.append(aggregation.detach().cpu().numpy())
+			gt.append(labels.cpu().numpy())
 
-    results = np.concatenate(results, axis=0)
-    gt = np.concatenate(gt, axis=0)
-    results[results <= 0] = 0
-    results[results > 0] = 1
-    gt[gt == 0] = 1
-    gt[gt == -1] = 0
+	results = np.concatenate(results, axis=0)
+	gt = np.concatenate(gt, axis=0)
+	results[results <= 0] = 0
+	results[results > 0] = 1
+	gt[gt == 0] = 1
+	gt[gt == -1] = 0
 
-    # For each class
-    precision = dict()
-    recall = dict()
-    average_precision = dict()
-    for i in range(n_classes):
-        precision[i], recall[i], _ = precision_recall_curve(
-            results[:, i], gt[:, i])
-        average_precision[i] = average_precision_score(
-            results[:, i], gt[:, i])
+	# For each class
+	precision = dict()
+	recall = dict()
+	average_precision = dict()
+	for i in range(n_classes):
+		precision[i], recall[i], _ = precision_recall_curve(
+			results[:, i], gt[:, i])
+		average_precision[i] = average_precision_score(
+			results[:, i], gt[:, i])
 
-    # A "micro-average": quantifying score on all classes jointly
-    precision["micro"], recall["micro"], _ = precision_recall_curve(results.ravel(),
-                                                                    gt.ravel())
-    average_precision["micro"] = average_precision_score(results, gt,
-                                                         average="micro")
+	# A "micro-average": quantifying score on all classes jointly
+	precision["micro"], recall["micro"], _ = precision_recall_curve(results.ravel(),
+																	gt.ravel())
+	average_precision["micro"] = average_precision_score(results, gt,
+														 average="micro")
 
-    for i in range(n_classes):
-        print('class:{} precision:{}'.format(class_names[i], precision[i]))
-        print('class:{} recall:{}'.format(class_names[i], recall[i]))
-        print('class:{} average_precision:{}'.format(
-            class_names[i], average_precision[i]))
-    print('avg precision:{} avg recall:{} avg average_precision:{}'.format(
-        precision["micro"], recall["micro"], average_precision["micro"]))
+	for i in range(n_classes):
+		print('class:{} precision:{}'.format(class_names[i], precision[i]))
+		print('class:{} recall:{}'.format(class_names[i], recall[i]))
+		print('class:{} average_precision:{}'.format(
+			class_names[i], average_precision[i]))
+	print('avg precision:{} avg recall:{} avg average_precision:{}'.format(
+		precision["micro"], recall["micro"], average_precision["micro"]))
 
 
 # CUDA_VISIBLE_DEVICES=0,1 python -m pdb main.py
 # scipy 1.2.0 python 3.6.6
 if __name__ == '__main__':
-    args = parse()
-    train_infer(args)
+	args = parse()
+	train_infer(args)
