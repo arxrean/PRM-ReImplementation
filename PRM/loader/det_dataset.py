c832a9e69845b4c37ffb07f4fc6b03a8cf1b0e9f
arxrean <602052254@qq.com>
2019-06-25 18:15:08 +0800

    abc

--- PRM/loader/det_dataset.py
+++ PRM/loader/det_dataset.py
@@ -132,7 +132,7 @@ class PascalVOCCount(data.Dataset):
     def __getitem__(self, idx):
         pdb.set_trace()
         img_name = self.img_list[idx]
-        img_path = os.path.join(self.args.voc12_root, str(img_name)[:4]+'_'+str(img_name)[4:]))+'.jpg'
+        img_path = os.path.join(self.args.voc12_root, str(img_name)[:4]+'_'+str(img_name)[4:])+'.jpg'
         img = Image.open(img_path).convert('RGB')
         img_trans = self.transform(img)
 
